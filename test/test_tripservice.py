ca0631f4461fd2120664ae2084c6ba01fbec3442
Bert Huisman <bert.huisman@ns.nl>
2024-07-03 02:53:17 +0200

    Initial commit

---
+++ test/test_tripservice.py
@@ -0,0 +1,9 @@
+import pytest
+
+from service.tripservice import getTripsByUser, Trip, UserNotLoggedInException, _findTripsByUser, _getLoggedUser, _isUserLoggedIn
+
+if __name__ == "__main__":
+    pytest.main()
+
+   
+
